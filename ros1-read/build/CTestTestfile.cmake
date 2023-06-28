4f505b886e5e3a0de058505621f7769a2948b446
LHope <1610732780@qq.com>
2023-06-28 00:35:23 -0700

    用ros1，放弃使用ros2读取imu数据，ros2需要转换bag包，过于繁杂

---
+++ ros1-read/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/lhope/Documents/ros-myb/ros1-read/src
+# Build directory: /home/lhope/Documents/ros-myb/ros1-read/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("imu_read")
