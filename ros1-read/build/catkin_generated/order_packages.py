ba861ded4b1871e16d2b0605ffef4b136fc9c963
LHope <1610732780@qq.com>
2023-06-29 03:03:51 -0700

    bucong

--- ros1-read/build/catkin_generated/order_packages.py
+++ ros1-read/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/lhope/Documents/ros-myb/ros1-read/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/lhope/Documents/ros-myb/ros1-read/devel;/opt/ros/noetic'.split(';') if '/home/lhope/Documents/ros-myb/ros1-read/devel;/opt/ros/noetic' != '' else []
+underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
--- ros1-read/build/catkin_generated/setup_cached.sh
+++ ros1-read/build/catkin_generated/setup_cached.sh
@@ -7,4 +7,9 @@
 # new environment variables
 
 # modified environment variables
+export CMAKE_PREFIX_PATH="/home/lhope/Documents/ros-myb/ros1-read/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/lhope/Documents/ros-myb/ros1-read/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/lhope/Documents/ros-myb/ros1-read/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/lhope/Documents/ros-myb/ros1-read/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/lhope/Documents/ros-myb/ros1-read/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/lhope/Documents/ros-myb/ros1-read/src:$ROS_PACKAGE_PATH"
