791ff7f649e1d28da2f36d94aa8c337dbb5c725a
LHope <1610732780@qq.com>
2023-06-29 03:34:11 -0700

    depth_camera

---
+++ ros1-read/build/depth_camera/CMakeFiles/camera_read_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/lhope/Documents/ros-myb/ros1-read/devel/lib/depth_camera/camera_read_node"
+  "/home/lhope/Documents/ros-myb/ros1-read/devel/lib/depth_camera/camera_read_node.pdb"
+  "CMakeFiles/camera_read_node.dir/src/camera_read.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/camera_read_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ros1-read/build/depth_camera/CMakeFiles/depth_camera_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/lhope/Documents/ros-myb/ros1-read/devel/lib/depth_camera/depth_camera_node"
+  "/home/lhope/Documents/ros-myb/ros1-read/devel/lib/depth_camera/depth_camera_node.pdb"
+  "CMakeFiles/depth_camera_node.dir/src/depth_camera.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/depth_camera_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
